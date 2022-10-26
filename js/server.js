bb8a2847faa72199885bf47020d6569780d479ac
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2022-10-26 13:51:32 -0400

    diffent  roots

--- js/server.js
+++ js/server.js
@@ -1,10 +1,21 @@
 var http=require('http')
 var fs=require('fs')
-const { type } = require('os')
 http.createServer(function(req,res){
+    if(req.url==='/'){
     fs.readFile('index.html',function(err,data){
         res.writeHead(200,{'content-type':'text/html'})
         res.write(data)
         res.end()
-    })
-}).listen(8000)
+    })}else if(req.url==='/login'){
+        res.write('login')
+        res.end()
+    }
+    else{
+        res.write('error')
+        res.end()
+    }
+
+}).listen(8000,() =>console.log("serverstarted")
+
+)
+
