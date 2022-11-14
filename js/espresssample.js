68e3b8fb88e755a19318f1344cd97f7d160d3374
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2022-11-14 10:23:53 -0500

    get in express

--- js/espresssample.js
+++ js/espresssample.js
@@ -1,10 +1,15 @@
 const express=require('express')
+const path=require('path')
 const app=express()
+
 app.get('/',function(req,res){
-    res.send('sajith')
+    res.sendFile(path.join(__dirname,'sign.html'))
 })
+app.post('/sign')
 
 
 app.listen(3000,function(){
     console.log("hai")
+    console.log(__dirname)
+    console.log(__filename)
 })
